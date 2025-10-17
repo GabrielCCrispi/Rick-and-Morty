eafd0806a1012cdd0ffca79b3e3a3651d40d65eb
Crispi <GabrielCrispi02@hotmail.com>
2025-10-16 23:21:08 -0300

    adicionar imports TypeORM nos módulos de auth e my-characters

--- src/my-characters/my-characters.module.ts
+++ src/my-characters/my-characters.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MyCharactersController } from './my-characters.controller';
 import { MyCharactersService } from './my-characters.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FavoriteCharacter } from './favorite-character.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FavoriteCharacter])], 
   controllers: [MyCharactersController],
   providers: [MyCharactersService]
 })

8660836fa4a2222abc401f7a9fed61bfa64574ff
Crispi <GabrielCrispi02@hotmail.com>
2025-10-17 22:09:34 -0300

    Correções de meus personagens

--- src/my-characters/my-characters.module.ts
+++ src/my-characters/my-characters.module.ts
@@ -3,9 +3,10 @@ import { MyCharactersController } from './my-characters.controller';
 import { MyCharactersService } from './my-characters.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoriteCharacter } from './favorite-character.entity';
+import { User } from '../auth/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FavoriteCharacter])], 
+  imports: [TypeOrmModule.forFeature([FavoriteCharacter, User])],
   controllers: [MyCharactersController],
   providers: [MyCharactersService]
 })
